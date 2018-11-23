60347905f40804ca34782836ceefd61c3d920de5
Itaru <litorud@live.jp>
2018-11-23 18:11:42 +0900

    二重起動防止&既存ウィンドウアクティブ化の処理をメモリマップトファイル+SetForegroundWindow で実現しました。

--- DesktopBackgroundScribbler/App.xaml.cs
+++ DesktopBackgroundScribbler/App.xaml.cs
@@ -1,32 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
 using System.IO;
-using System.Linq;
-using System.ServiceModel;
+using System.IO.MemoryMappedFiles;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Threading;
+using System.Windows.Interop;
 
 namespace DesktopBackgroundScribbler
 {
     /// <summary>
     /// App.xaml の相互作用ロジック
     /// </summary>
-    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single, UseSynchronizationContext = false)]
-    public partial class App : Application, IActivatable
+    public partial class App : Application
     {
         const string id = "{14482529-941C-4025-80F8-1836D76B064B}";
-        const string baseAddress = "net.pipe://localhost/DesktopBackgroundScribbler";
+        const string memoryMappedFileName = id + ".dat";
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
 
         [STAThread]
         public static void Main()
         {
             using (var semaphore = new Semaphore(1, 1, id, out var createdNew))
             {
+                AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+
                 if (!createdNew)
                 {
                     ActivateExistingWindow();
@@ -34,71 +34,51 @@ namespace DesktopBackgroundScribbler
                 }
 
                 var app = new App();
+                app.InitializeComponent();
 
-                // http://nekojarashi.hatenablog.jp/entry/2017/08/14/WCF_%E3%81%A7%E7%B0%A1%E5%8D%98%E3%81%AB%E3%83%97%E3%83%AD%E3%82%BB%E3%82%B9%E9%96%93%E9%80%9A%E4%BF%A1
-                // https://docs.microsoft.com/ja-jp/dotnet/framework/wcf/migrating-from-net-remoting-to-wcf
-                // この辺参考にした。
-                using (var serviceHost = new ServiceHost(app, new Uri(baseAddress)))
+                var mainWindow = new MainWindow();
+
+                using (var mmf = MemoryMappedFile.CreateNew(memoryMappedFileName, 8))
                 {
-                    serviceHost.AddServiceEndpoint(
-                        typeof(IActivatable),
-                        new NetNamedPipeBinding(),
-                        baseAddress);
-                    serviceHost.Open();
+                    mainWindow.Loaded += (sender, e) =>
+                    {
+                        var windowHandle = new WindowInteropHelper(mainWindow).Handle.ToInt64();
 
-                    AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+                        using (var stream = mmf.CreateViewStream())
+                        {
+                            var binaryWriter = new BinaryWriter(stream);
+                            binaryWriter.Write(windowHandle);
+                        }
+                    };
 
-                    app.InitializeComponent();
-                    app.Run();
+                    app.Run(mainWindow);
                 }
             }
         }
 
         private static void ActivateExistingWindow()
         {
-            var binding = new NetNamedPipeBinding();
-            var address = new EndpointAddress(baseAddress);
-            using (var channelFactory = new ChannelFactory<IActivatable>(binding, address))
+            var count = 0;
+            do
             {
-                var activatable = channelFactory.CreateChannel();
-
-                var count = 0;
-                do
+                try
                 {
-                    try
+                    using (var mmf = MemoryMappedFile.OpenExisting(memoryMappedFileName))
+                    using (var stream = mmf.CreateViewStream(0, 8, MemoryMappedFileAccess.Read))
                     {
-                        if (activatable.Activate())
+                        var binaryReader = new BinaryReader(stream);
+                        var windowHandle = binaryReader.ReadInt64();
+                        if (windowHandle > 0)
                         {
-                            break;
+                            SetForegroundWindow(new IntPtr(windowHandle));
+                            return;
                         }
                     }
-                    catch (EndpointNotFoundException)
-                    {
-                        // EndpointNotFoundException が発生した IClientChannel は、状態が Faulted になり、
-                        // 以降、通信や Close() を実行しても、CommunicationObjectFaultedException になる。
-                        // そのため、新しいインスタンスを activatable に代入する必要がある。
-                        // なお、元のインスタンスは Abort() でリソースを解放する必要がある。
-                        // https://docs.microsoft.com/ja-jp/dotnet/api/system.servicemodel.icommunicationobject.state?view=netframework-4.7.2#System_ServiceModel_ICommunicationObject_State
-                        // 解放しなかった場合、ChannelFactory の Dispose() 時にも CommunicationObjectFaultedException が発生する。
-                        // Abort() を呼び出したからといって再利用できるようになるわけではない。
-                        (activatable as IClientChannel)?.Abort();
-                        activatable = channelFactory.CreateChannel();
-                    }
+                }
+                catch (FileNotFoundException) { }
 
-                    // 最近の Windows は、入力中に突然別のウィンドウにフォーカスが奪われたりすることの無いように、
-                    // Activate() を呼び出してもアクティブにせず、タスクバーアイコンを点滅させるだけで false を返すことがある。
-                    // このような場合、以下の Sleep() が無いと、大量に Activate() を呼び出してしまい、タスクバーアイコンの点滅がちらつく。
-                    // これを防ぐため、Sleep() で1秒おきにしか Activate() を呼び出さないようにしている。
-                    Thread.Sleep(1000);
-                } while (++count < 10);
-
-                (activatable as IClientChannel)?.Close();
-            }
-        }
-
-        public bool Activate()
-        {
-            return Dispatcher?.Invoke(() => MainWindow?.Activate()) ?? false;
+                Thread.Sleep(1000);
+            } while (++count < 10);
         }
 
         public static void Log(object message)
